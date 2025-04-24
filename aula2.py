2d18ea0bc2440d3ccab26eed94f29c9dac411738
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-24 09:24:12 -0300

    first steeps in python

---
+++ aula2.py
@@ -0,0 +1,15 @@
+# sep='', separador entre as palavras
+print(12, 34, 1011, sep='-') 
+print(56, 78, sep='*')
+print(9, 10, sep='**')
+
+# caractere de quebra de linha \n = CRLF
+# \n -> LF unix
+
+print(107, 55, 105, sep='-', end='\n##')
+
+"""
+Mais uma coisa a ter em mente é que em Python
+diferencia letras maiuscla de letras minusculas
+
+"""
---
+++ aula4.py
@@ -0,0 +1,21 @@
+# Tipo int e float
+# int -> Número inteuro
+# O tipo int representa qualquer número
+# positivo ou negativo. int sem sinal é considerado
+# positivo.
+print(11) # int
+print(-11) # int
+print(0) # int
+
+# float -> Número com ponto flutuante
+# O tipo float repesenta qualquer número
+# positivo ou negativo com ponto flutuante.
+# float sem sinal é considerado positivo
+print(1.5, 10.11) # float
+print(0.0, -1.2)
+
+# A função type mostra o tipo que o Python
+# inferior ao valor
+print(type(10))
+print(type(10.5), type(1.2), type(0.0))
+print(type('Paulo'))
