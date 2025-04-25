d47dcc3ea38442f3dd27686f70e9051c5365a5b0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-25 00:38:56 -0300

    operações aritméticas com python

---
+++ 1.logicao_programacao/aula9.py
@@ -0,0 +1,25 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtração = 10 - 5
+print('Subtração', subtração)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2
+print('Divisao', divisao)
+
+divisao_inteiro = 10 // 2.2
+print('Divisão inteira', divisao_inteiro)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('Mudlo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
