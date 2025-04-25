9b3942efec8c8d9d9c8f6536bb8a6e76b4428821
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-25 13:55:46 -0300

    introdução a formatação de string com f.strings

---
+++ 1.logicao_programacao/aula13.py
@@ -0,0 +1,17 @@
+nome = 'Paulo Carvalho'
+altura = 1.80
+peso = 96
+imc = peso / (altura * altura)
+
+
+linha_1 = f'{nome} tem {altura:.2f} de altuera'
+linha_2 = f'pesa {peso} quilos e seu IMC é'
+linha_3 = f'{imc}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+# nome tem 1.80 de altura
+# peso quilos e seu IMC é
+# 29.32098765432987
