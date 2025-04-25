46b0ff0e5c3b6f1b382230008ef6229e2dd5a462
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-25 13:47:48 -0300

    exercicio de imc resolvido python basico

---
+++ 1.logicao_programacao/aula12.py
@@ -0,0 +1,10 @@
+nome = 'Paulo Carvalho'
+altura = 1.80
+peso = 96
+imc = peso / (altura * altura)
+
+# nome tem 1.80 de altura
+# peso quilos e seu IMC é
+# 29.32098765432987
+
+print(f'{nome} tenho {altura} de altura peso {peso} quilos e meu IMC é {imc:.2f}' )
