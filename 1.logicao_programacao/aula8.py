a536bef2dd09fb68c7e1cce4b9bd09e83822d1cf
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-24 23:09:40 -0300

    primeiro exercicio de tipos de dados em python

---
+++ 1.logicao_programacao/aula8.py
@@ -0,0 +1,16 @@
+# PRIMEIRO EXERCICIOS DO CURSO VARIÁVEIS TIPOS DE DADOS
+
+# SETANDO AS VARIÁVEIS
+nome = 'Paulo'
+sobrenome = 'Carvalho'
+idade = 17
+ano_de_nascimento = 2025 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.80
+
+print('nome:', nome)
+print('sobrenome:',sobrenome)
+print('idade:',idade)
+print('ano de nascimento:', ano_de_nascimento)
+print('maior de idade:', maior_de_idade)
+print('altura em metros:', altura_metros)
