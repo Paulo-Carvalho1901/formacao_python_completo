1081be0a1a24757f0490cd29d766c8c2f603b0e2
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-24 22:52:45 -0300

    introdução de variavel e sua utilização

---
+++ 1.logicao_programacao/aula7.py
@@ -0,0 +1,19 @@
+# Variáveis são usadas para salvar algo na memoria do computador
+#PEP8: Inicie variaveis com letras maiusculas, pode usar
+# números e underline_
+# O sinal de = o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+# nome_completo = 'Paulo Carvalho'
+# soma_dois_mais_dois = 2 + 2
+# int_um = int('1')
+# print(int_um, type(int_um))
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Paulo'
+idade = 30
+maior_de_idade = idade >= 18
+
+print('nome: ', nome, 'idade: ', idade)
+print('É maior: ', maior_de_idade)
