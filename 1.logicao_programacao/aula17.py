778284739c371d24a56fe9233aaf3d0d18919f1f
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-29 15:40:48 -0300

    condições com if varios blocos de códigos entendendo a lógica

--- 1.logicao_programacao/aula17.py
+++ 1.logicao_programacao/aula17.py
@@ -1,9 +1,25 @@
 # if / elif       / else
 # se / se não se  / se não 
 
-condicao = True # Criando uma condição
+# Criando uma condição
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
 
-if condicao:
-    print('esse código é o bloco do if')
+if condicao1:
+    print('Código para condição 1')
+    print('pode ter varias linhas de código')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nemhuma condição foi satisfeita')
+
+if 10 == 10:
+    print('outro if')
 
 print('FORA DO BLOCO')
