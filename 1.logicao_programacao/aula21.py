870de84ed149c192985d7afd530db0140d3ba0f0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-30 13:34:37 -0300

    avaliação de curto circuito em python entendendo seu funcionamento

--- 1.logicao_programacao/aula21.py
+++ 1.logicao_programacao/aula21.py
@@ -9,12 +9,16 @@
 # Também existe o tipo None que é 
 # usado para representar um não valor
 
-entrada = input('[E]ntrar [S]air: ')
-senha_digitada = input('Senha: ')
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
 
-senha_permitida = '1234'
-if entrada == 'E' and senha_digitada == senha_permitida:
-    print('Entrar')
-else:
-    print('Sair')
+# senha_permitida = '1234'
+# if entrada == 'E' and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
 
+# RETORNO DA CONDIÇÃO AND
+# Avaliação de curto circuito
+print(True and False and True)
+print(True and 0 and True)
