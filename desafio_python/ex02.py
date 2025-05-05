48f63c3c8467029cddf2263fd8815a24699b6b78
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-05 09:50:13 -0300

    entrada de dados solicitando dia mes e ano de anascimento

---
+++ desafio_python/ex02.py
@@ -0,0 +1,11 @@
+"""
+Crie um script python que leia o dia,
+o mês e o ano de nascimento de uma pessoa
+e mostre uma mensagem com a data formatada
+"""
+
+dia = input('Informe o dia: ')
+mes = input('Informe o mês: ')
+ano = input('informe o ano: ')
+
+print(f'Você nasceu no dia {dia} do mês {mes} e no ano {ano}')
