8a02bec39b7983265a0ae2196b8cf301084ede0c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-05 02:58:14 -0300

    operador not inverte sua aperação

---
+++ 1.logicao_programacao/aula23.py
@@ -0,0 +1,17 @@
+# Operador lógico "not"
+# Usado para inverter expresoões
+# not True = False
+# not False = True
+
+senha = input('senha: ')
+
+
+# if senha != '1234':
+#     print('Senha incorreta')
+
+# if not senha:
+#     print('Você não digitou nada')
+
+print(not True)
+print(not 0)
+print(not 1)
