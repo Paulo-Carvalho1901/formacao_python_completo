f6427b171ae7fc0ea1700c5c777a3a0f7e88bd18
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-05 09:46:46 -0300

    ex01 ebtrada de dados nome msg boas vindas

---
+++ desafio_python/ex01.py
@@ -0,0 +1,8 @@
+"""
+Crie um script python que leia o nome de uma pessoa
+e mostre umna mensagem de boas-vindas
+de acordo com valor digitado
+"""
+
+nome = input('Digite seu nome: ')
+print(f'Olá, {nome} seja Bem-vindo ao nosso site.')
