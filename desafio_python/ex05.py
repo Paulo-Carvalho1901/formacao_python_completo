7f9c52b0ca225cc41b657aeffb4a161b7aed7bad
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-05 10:08:17 -0300

    numero antecessor e sucessor

---
+++ desafio_python/ex05.py
@@ -0,0 +1,9 @@
+"""
+Faça um programa que que leia um numero inteiro
+e mostre na tela seu sucessor e seu antecessor
+"""
+numero = int(input('Digite um números: '))
+antec = numero - 1
+suces = numero + 1
+
+print(f'Seu número foi {numero} seu antecessor foi {antec} e seu sucessor foi {suces}')
