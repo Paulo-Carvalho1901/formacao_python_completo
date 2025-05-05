a4b68daeb46565af0bc3e8839e3403ef6db27d64
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-05 09:53:19 -0300

    soma de dois numeros

---
+++ desafio_python/ex03.py
@@ -0,0 +1,11 @@
+"""
+Crie um script python
+que leia duas numeros
+e tenta mostrar a soma entre eles
+"""
+
+numero_1 = int(input('Digite o primeiro números: '))
+numero_2 = int(input('Digite o segundo número: '))
+
+soma = numero_1 + numero_2
+print(f'A soma dos números é {soma}')
