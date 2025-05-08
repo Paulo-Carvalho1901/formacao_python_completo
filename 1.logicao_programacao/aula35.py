0d168127cb100c3782e0db4ad4ca4a988be7171d
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-07 23:32:39 -0300

    continuando a entender o while e suas condições

---
+++ 1.logicao_programacao/aula35.py
@@ -0,0 +1,15 @@
+"""
+Repetição
+while (enquanto)
+Executar uma ação enqunto uma condição for verdadeira
+Loop infinito quando um código não tem fim 
+"""
+
+contador = 0 # setando o incio da variavel em zero
+
+while contador < 10: # criando a condição de check do código
+    contador = contador + 1 # encrementando a variavel em 1
+    print(contador) # mostrando o resultado
+    
+
+print('Acabou!')
