57e14ac670c44cb627dca310f0b3f8e8bf0c6cfc
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-08 23:01:07 -0300

    add explicação no código

--- 1.logicao_programacao/aula39.py
+++ 1.logicao_programacao/aula39.py
@@ -8,12 +8,12 @@ nome = 'Paulo Carvalho' # iterável
 # print(tamanho_str)
 # print(nome[3])
 
-indice = 0 
-novo_nome = ''
-while indice < len(nome):
-    letra = nome[indice]
-    novo_nome += f'*{letra}'
-    indice += 1
+indice = 0 # criando uma indice 0
+novo_nome = '' # criando uma variável novo_nome
+while indice < len(nome): # aqui enquanto o indice for menor que o tamanho da string
+    letra = nome[indice] # aqui criei uma varia letra que recebe cada letra na string uma a uma e salva dentro dela
+    novo_nome += f'*{letra}' # aqui nesse trecho acontece acumulaçao da variável vazia novo_nome com operador de atribuição
+    indice += 1 # aqui soma o indice mais ele mesmo para não ter um loop infinito
 
 novo_nome += '*'
-print(novo_nome)
+print(novo_nome) # imprimindo o resultado
