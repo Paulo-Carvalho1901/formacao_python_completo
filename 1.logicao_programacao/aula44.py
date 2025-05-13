ea8285acf3ae3e4b8ed778b7c63eeb08d9f9ca81
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-13 08:58:00 -0300

    range for comecando as interações

---
+++ 1.logicao_programacao/aula44.py
@@ -0,0 +1,23 @@
+# senha_secreta = '12' # str
+# senha_digitada = ''
+# repeticoes = 0
+
+# while senha_secreta != senha_digitada:
+#     senha_digitada = input(f'Sua senha ({repeticoes}x): ') # str
+
+#     repeticoes += 1
+
+# print(repeticoes)
+# print('Laço continuara iternamente')
+
+
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+numeros = range(10)
+# numero = range(5, 10)
+# numero = range(5, 10, 2)
+
+for numero in numeros:
+    print(numero)
