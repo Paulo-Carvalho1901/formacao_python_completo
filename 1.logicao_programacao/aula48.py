8e502d3f84c6065489ec14d7cb47d6d99637c8a0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-14 11:33:24 -0300

    lista insert na lista no final ou em qualquer parte da lista pop da lista

--- 1.logicao_programacao/aula48.py
+++ 1.logicao_programacao/aula48.py
@@ -10,13 +10,10 @@ Criar, ler, Alterar, apagar, = lista[i] (CRUD)
 
 #        0   1   2   3   4   5
 lista = [10, 20, 30, 40]
-# lista[2] = 300
-# del lista[2]
-# print(lista)
-# print(lista[2])
-lista.append(50)
-lista.pop()
-lista.append(60)
-lista.append(70)
-utilmo_valor = lista.pop(3)
-print(lista, 'Rmovido', utilmo_valor)
+lista.append('Paulo')
+nome = lista.pop()
+lista.append(1233)
+del lista[-1]
+# lista.clear()
+lista.insert(100, 5)
+print(lista)
