251d6254b33453fdc33daa65c86dd3da7fe56ac0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-14 10:54:52 -0300

    introdução ao tipo lista

---
+++ 1.logicao_programacao/aula48.py
@@ -0,0 +1,21 @@
+"""
+Listas em Python
+Tipo list - mutável
+Suporta vários valores de qualquer tipo
+conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, top, del, clear, extend +
+"""
+
+#         12345 
+#        -54321
+string = 'ABCDE' # 5 Caracteres
+
+# print(lista, type(lista))
+# print(bool([])) # False
+
+#        0    1      2                3    4
+lista = [123, True, 'Paulo Carvalho', 1.2, []] # ''
+# print(lista[2], type(lista[2])) # verificando o tipo da lista
+
+lista[2] = 'Maria'
+print(lista)
