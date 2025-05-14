8c998b21ed7cd3cf95e710b426fb4a9973992716
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-14 10:22:46 -0300

    media escolar

---
+++ desafio_python/ex07.py
@@ -0,0 +1,11 @@
+"""
+Desenvolva um programa que leia as duas notas de um aluno
+e calcule sua média
+"""
+
+nota_1 = int(input('Insira a nota 1: '))
+nota_2 = int(input('Insira a nota 2: '))
+
+media = (nota_1 + nota_2) / 2
+
+print(f'A sua média escolar é {media}')
