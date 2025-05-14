bbe7c55822d4c413262b6461ecea1c845e3ceb1c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-14 11:53:20 -0300

    listas metodo copy para copia os valores em outra variavel sem altera a variavel base

--- 1.logicao_programacao/aula48.py
+++ 1.logicao_programacao/aula48.py
@@ -1,15 +1,20 @@
 """
-Listas em Python
-Tipo list - mutável
-Suporta vários valores de qualquer tipo
-conhecimentos reutilizáveis - índices e fatiamento
-Métodos úteis: append, insert, top, del, clear, extend +
-Create Read Update Delete
-Criar, ler, Alterar, apagar, = lista[i] (CRUD)
+Cuidado com dados mutáveis
+= - copiado o valor (imutável)
+= - aponta para o mesmo valor na memória (mutável)
 """
+# nome = 'Paulo'
+# outro_nome = nome
+# nome = 'João'
 
-lista_a = [1, 2, 3]
-lista_b = [4, 5, 6]
-lista_c = lista_a + lista_b
-lista_a.extend(lista_b)
+# print(nome)
+# print(outro_nome)
+
+
+lista_a = ['Paulo', 'Davi', 1, True, 1.2]
+lista_b = lista_a.copy()
+
+lista_a[0] = 'Qualquer coisa'
 print(lista_a)
+print(lista_b)
+
