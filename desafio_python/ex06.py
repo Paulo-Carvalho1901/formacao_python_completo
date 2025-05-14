60d9a341a1b732307c6ea0b66b0d473d96bf9e1f
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-14 10:15:31 -0300

    exercicio dobro triplo e raiz

---
+++ desafio_python/ex06.py
@@ -0,0 +1,15 @@
+"""
+Crie um algoritmo que leia um número e
+mostre seu dobro triplo e raiz quadrada
+
+"""
+
+numero = int(input('Digite um número: '))
+
+dubro = numero * 2
+triplo = numero * 3
+raiz = numero ** (1/2)
+
+print(dubro)
+print(triplo)
+print(f'{raiz:.2f}')
