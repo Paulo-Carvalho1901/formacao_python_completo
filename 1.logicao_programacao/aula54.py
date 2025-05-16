610be82b382f71e30adc081770b8bd9013faaecc
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-16 14:15:17 -0300

    Iniciando exercicio de lista de compra

---
+++ 1.logicao_programacao/aula54.py
@@ -0,0 +1,8 @@
+"""
+Faça uma lista de compra com listas
+o usuario deve ter a possibilidade de 
+inserir, apagar, e listar valores de sua lista
+Não permita que o programa quebre com 
+error de índices inexistentes na lista
+"""
+
