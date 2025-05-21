05a39553b20885300b65157adef8053e6bc12980
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-21 17:26:45 -0300

    criado estrutura de condição if elif e else para as opções controlando o fluxo do programa

--- 1.logicao_programacao/aula54.py
+++ 1.logicao_programacao/aula54.py
@@ -9,6 +9,17 @@ error de índices inexistentes na lista
 lista = []
 
 while True:
+    # criadno condição para interação com usuário
     print('Selecione uma opção:')
     opcao = input('[i]nserir [a]pagar [l]istar: ')
+
+    # criando estrutura de controle para as opções
+    if opcao == 'i':
+        print('i')
+    elif opcao == 'a':
+        print('a')
+    elif opcao == 'l':
+        print('l')
+    else:
+        print('Por favor digite uma opção válida. ')
     
