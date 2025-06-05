79d1c2d3aee4c37bf46c78a3a7d0f431c596e889
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-05 18:06:34 -0300

    criado calculo segundo digito cpf

--- 1.logicao_programacao/aula62.py
+++ 1.logicao_programacao/aula62.py
@@ -29,9 +29,19 @@ nove_digitos = cpf[:9] # pegando apenas os 9 digtos
 conator_regressivo_1 = 10 # criado um contador
 
 resultado_digito_1 = 0 
-for digito_1 in nove_digitos: # iterando sobre novo_digito
-    resultado_digito_1 += int(digito_1) * conator_regressivo_1 # fzendo calculo 
+for digito in nove_digitos: # iterando sobre novo_digito
+    resultado_digito_1 += int(digito) * conator_regressivo_1 # fzendo calculo 
     conator_regressivo_1 -= 1 # fazendo o decremento contagem regressiva
 digito_1 = (resultado_digito_1 * 10) % 11 
 digito_1 = digito_1 if digito_1 <= 9 else 0 # Operação ternária
+
+dez_digito = nove_digitos + str(digito_1)
+conator_regressivo_2 = 11
+
+resultado_digito_2 = 0
+for digito in dez_digito:
+    resultado_digito_2 += int(digito) * conator_regressivo_2
+    conator_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
 
