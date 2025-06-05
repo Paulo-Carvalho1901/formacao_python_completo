8795a289d4fa5c31b74b92266cfad7cf59c55f02
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-05 15:40:32 -0300

    inicio de operação  ternária

---
+++ 1.logicao_programacao/aula60.py
@@ -0,0 +1,8 @@
+"""
+Operação ternária (condicional em linha)
+<valor> if <condicao> else <outro valor>
+"""
+
+condicao = 10 == 10
+variavel = 'valor' if condicao else 'Outro valor'
+print(variavel)
