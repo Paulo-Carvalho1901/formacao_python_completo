cbdf0af6cbae188161ef69d3ef33478c36484f3c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-05 15:47:14 -0300

    exemplo de operação ternária

--- 1.logicao_programacao/aula60.py
+++ 1.logicao_programacao/aula60.py
@@ -3,6 +3,11 @@ Operação ternária (condicional em linha)
 <valor> if <condicao> else <outro valor>
 """
 
-condicao = 10 == 10
-variavel = 'valor' if condicao else 'Outro valor'
-print(variavel)
+# condicao = 10 == 10
+# variavel = 'valor' if condicao else 'Outro valor'
+# print(variavel)
+
+digito = 9 
+novo_digito = digito if digito <= 9 else 0
+novo_digito = 0 if digito > 0 else digito
+print(novo_digito)
